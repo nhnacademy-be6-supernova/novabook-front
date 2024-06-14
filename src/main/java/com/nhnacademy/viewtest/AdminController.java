d39c0ea81212d4adaa5fe329b99ba99139f19613
hyungwang <kfk10112@gmail.com>
2024-06-14 16:10:50 +0900

    admin_page

---
+++ src/main/java/com/nhnacademy/viewtest/AdminController.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.viewtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String home() {
+        return "admin_page";
+    }
+}
