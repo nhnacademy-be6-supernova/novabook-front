096578df262773ff6dc23201eb2f84b5bfd6c7d3
hyungwang <kfk10112@gmail.com>
2024-06-14 20:51:40 +0900

    admin_page

--- src/main/java/com/nhnacademy/viewtest/AdminController.java
+++ src/main/java/com/nhnacademy/viewtest/AdminController.java
@@ -1,12 +1,25 @@
 package com.nhnacademy.viewtest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
-    @GetMapping("/admin")
-    public String home() {
+    @GetMapping("/admin/coupon")
+    public String getCoupon() {
         return "admin_coupon_type";
+    }
+
+    @PostMapping("/admin/coupon")
+    public String postCoupon(@RequestParam("selectedItem") String selectedItem,
+                             @RequestParam("adminSubmittedValue") String adminSubmittedValue,
+                             @RequestParam("selectedAdditionalItem") String selectedAdditionalItem,
+                             Model model) {
+
+        model.addAttribute("adminSubmittedValue", adminSubmittedValue);
+        return "admin_coupon_detail";
     }
 }
