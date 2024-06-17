4d2f019127cd562a39ee3bf0085252980e2f2a76
zaysverse <zeze@jbnu.ac.kr>
2024-06-17 16:05:30 +0900

    ci: /제거

--- src/main/java/com/nhnacademy/novabook_front/store/book/BookController.java
+++ src/main/java/com/nhnacademy/novabook_front/store/book/BookController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/books")
+@RequestMapping("books")
 
 @Controller
 public class BookController {
