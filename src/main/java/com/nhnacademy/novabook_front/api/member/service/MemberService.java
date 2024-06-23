5fc11d653f014ba76c2c602a7aa447148c5cd4fc
munyeongkim2 <wgansdud2@naver.com>
2024-06-23 14:24:24 +0900

    #65 feat : 로그인 구현

--- src/main/java/com/nhnacademy/novabook_front/api/member/service/MemberService.java
+++ src/main/java/com/nhnacademy/novabook_front/api/member/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.nhnacademy.novabook_front.api.member.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.nhnacademy.novabook_front.api.member.dto.CreateMemberRequest;
 import com.nhnacademy.novabook_front.api.member.dto.CreateMemberResponse;
+import com.nhnacademy.novabook_front.api.member.dto.LoginMemberRequest;
+import com.nhnacademy.novabook_front.api.member.dto.LoginMemberResponse;
 
 public interface MemberService {
 	CreateMemberResponse createMember(CreateMemberRequest createMemberRequest);
 
+	LoginMemberResponse login(LoginMemberRequest loginMemberRequest);
 }
