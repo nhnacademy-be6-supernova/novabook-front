feacfa8f0a595ddc2f17228cb089b3cbd9c8cf23
ganghagyeong <gkrrod714@naver.com>
2024-06-27 13:11:54 +0900

    #89 feat: 회원가입시 중복 검사 기능, dto validation

--- src/main/java/store/novabook/front/api/member/member/dto/CreateMemberRequest.java
+++ src/main/java/store/novabook/front/api/member/member/dto/CreateMemberRequest.java
@@ -1,18 +1,47 @@
 package store.novabook.front.api.member.member.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CreateMemberRequest(
+
+	@NotNull(message = "아이디는 필수 입력 값입니다.")
+	@Size(max = 20, message = "최대 20자까지 가능합니다.")
 	String loginId,
+
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[~!@#$%^&*]).{10,16}$")
+	@NotNull(message = "비밀번호는 필수 입력 값입니다.")
+	@Size(max = 150, message = "최대 150자까지 가능합니다.")
 	String loginPassword,
+
+	@NotNull(message = "이름은 필수 입력 값입니다.")
+	@Size(max = 50, message = "최대 50자까지 가능합니다.")
 	String name,
+
+	@NotNull(message = "연락처는 필수 입력 값입니다.")
+	@Size(max = 20, message = "최대 20자까지 가능합니다.")
 	String number,
+
+	@NotNull(message = "이메일은 필수 입력 값입니다.")
+	@Size(max = 100, message = "최대 100자까지 가능합니다.")
 	String email,
+
+	@NotNull(message = "이메일은 필수 입력 값입니다.")
 	String emailDomain,
+
+	@NotNull(message = "생년월일은 필수 입력 값입니다.")
 	Integer birthYear,
+
+	@NotNull(message = "생년월일은 필수 입력 값입니다.")
 	Integer birthMonth,
+
+	@NotNull(message = "생년월일은 필수 입력 값입니다.")
 	Integer birthDay,
+
+	@NotNull(message = "주소는 필수 입력 값입니다.")
 	String address) {
 
 	public String getEmailFull() {
@@ -20,4 +49,3 @@ public record CreateMemberRequest(
 	}
 
 }
-
