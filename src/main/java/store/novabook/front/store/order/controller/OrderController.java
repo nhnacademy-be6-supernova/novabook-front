97b08ee754f1aacbd211e60d1989a9f9c3473381
2-say <dev2say@gmail.com>
2024-06-27 16:21:25 +0900

    #97 refactor: sonarlint 코드 스멜 제거

--- src/main/java/store/novabook/front/store/order/controller/OrderController.java
+++ src/main/java/store/novabook/front/store/order/controller/OrderController.java
@@ -4,29 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import store.novabook.front.api.order.client.WrappingPaperClient;
-import store.novabook.front.store.order.dto.OrderTemporaryForm;
-import store.novabook.front.store.order.service.RedisOrderServive;
 
 @RequestMapping("/orders")
 @Controller
 public class OrderController {
 
-	private final WrappingPaperClient wrappingPaperClient;
-
-
-	public OrderController(WrappingPaperClient wrappingPaperClient) {
-		this.wrappingPaperClient = wrappingPaperClient;
-	}
-
 	@GetMapping("/order/form")
 	public String getOrderForm(Model model) {
-		// List<GetWrappingPaperResponse> papers = wrappingPaperClient.getWrappingPaperAllList().getBody().papers();
-		// model.addAttribute("papers", papers);
 		return "store/order/order_form";
 	}
 
