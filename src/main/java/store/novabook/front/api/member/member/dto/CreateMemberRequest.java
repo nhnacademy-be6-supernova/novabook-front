59860a35e0ae58a00a2a1b358028203611eee7c5
ganghagyeong <gkrrod714@naver.com>
2024-06-30 17:25:21 +0900

    #131 refactor: 비밀번호 변경시 암호화해서 db 저장, 회원가입시 프론트서버에서 하는 암호와 삭제

--- src/main/java/store/novabook/front/api/member/member/dto/CreateMemberRequest.java
+++ src/main/java/store/novabook/front/api/member/member/dto/CreateMemberRequest.java
@@ -18,6 +18,10 @@ public record CreateMemberRequest(
 	@Size(max = 150, message = "최대 150자까지 가능합니다.")
 	String loginPassword,
 
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Size(max = 150, message = "최대 150자까지 가능합니다.")
+	String loginPasswordConfirm,
+
 	@NotBlank(message = "이름은 필수 입력 값입니다.")
 	@Size(max = 50, message = "최대 50자까지 가능합니다.")
 	String name,
