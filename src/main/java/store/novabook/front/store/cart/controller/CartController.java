78b047d50e29389a380dadfc1779a7250e4cc15d
munyeongkim2 <wgansdud2@naver.com>
2024-07-04 10:43:59 +0900

    #144 CartController.java 수정

--- src/main/java/store/novabook/front/store/cart/controller/CartController.java
+++ src/main/java/store/novabook/front/store/cart/controller/CartController.java
@@ -43,43 +43,12 @@ public class CartController {
 
 		}
 
-		if (Objects.nonNull(cartCookie)) {
-			String uuid = String.valueOf(UUID.randomUUID());
-			Cookie cookie = new Cookie("cart", uuid);
-			response.addCookie(cookie);
-			model.addAttribute("cart", null);
-		}
+		// if (Objects.nonNull(cartCookie)) {
+		// 	String uuid = String.valueOf(UUID.randomUUID());
+		// 	Cookie cookie = new Cookie("cart", uuid);
+		// 	response.addCookie(cookie);
+		// 	model.addAttribute("cart", null);
+		// }
 		return "store/cart/cart_list";
 	}
-
-	// @GetMapping("/add/{id}")
-	// public String addCart(
-	// 	@CookieValue(name = "cart") Cookie cartCookie,
-	// 	@CookieValue(name = "Authorization") Cookie member,
-	// 	@PathVariable Long id,
-	// 	HttpServletResponse response,
-	// 	@ModelAttribute AddCartRequest addCartRequest,
-	// 	Model model) {
-	// 	// Authorization라는 쿠키가 있으면 로그인한 회원으로 인식하여 loginId를 쿠키 값으로 넣어준 뒤 쿠키 생성을 하고 응답 객체에 보낸다.
-	// 	if (Objects.nonNull(member)) {
-	// 		if (Objects.nonNull(cartCookie)) { // 로그인 안하고 장바구니에 상품을 넣었었음 기존 장바구니와 합치기 처리
-	//
-	// 		}
-	//
-	// 		GetCartResponse getCartResponse = cartService.getCartList();
-	//
-	// 		Cookie cookie = new Cookie("cart", getCartResponse.cartId().toString());
-	// 		response.addCookie(cookie);
-	//
-	// 		model.addAttribute("cart", getCartResponse);
-	// 	}
-	//
-	// 	if (Objects.nonNull(cartCookie)) {
-	// 		String uuid = String.valueOf(UUID.randomUUID());
-	// 		Cookie cookie = new Cookie("cart", uuid);
-	// 		response.addCookie(cookie);
-	// 		cartService.addBook(addCartRequest);
-	// 	}
-	//
-	// }
 }
