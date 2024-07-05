a137c6d23504b3ee8bf1c7c395c1752f694981df
Hantaehwan <gksxoghkskr@naver.com>
2024-07-05 14:05:05 +0900

    #145 feat, refactor: 리뷰 연결 완료 리뷰 관련 필요없는 코드 제거

--- src/main/java/store/novabook/front/api/delivery/dto/response/CreateDeliveryFeeResponse.java
+++ src/main/java/store/novabook/front/api/delivery/dto/response/CreateDeliveryFeeResponse.java
@@ -3,7 +3,6 @@ package store.novabook.front.api.delivery.dto.response;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateDeliveryFeeResponse(
-	@NotNull(message = "reviewId 값을 입력해주세요.")
 	Long id
 ) {
 }
