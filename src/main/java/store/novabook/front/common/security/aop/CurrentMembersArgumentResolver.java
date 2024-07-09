a8f59732290a6cf5a39c7d3938cf85cdf20cf24d
zaysverse <zeze@jbnu.ac.kr>
2024-07-09 12:36:39 +0900

    feat: 인증에 required 추가

--- src/main/java/store/novabook/front/common/security/aop/CurrentMembersArgumentResolver.java
+++ src/main/java/store/novabook/front/common/security/aop/CurrentMembersArgumentResolver.java
@@ -1,5 +1,7 @@
 package store.novabook.front.common.security.aop;
 
+import java.util.Objects;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -9,8 +11,12 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import store.novabook.front.api.member.member.service.MemberAuthClient;
+import store.novabook.front.common.exception.ErrorCode;
+import store.novabook.front.common.exception.UnauthorizedException;
 
+@Slf4j
 public class CurrentMembersArgumentResolver implements HandlerMethodArgumentResolver {
 
 	private final MemberAuthClient memberAuthClient;
@@ -21,27 +27,46 @@ public class CurrentMembersArgumentResolver implements HandlerMethodArgumentReso
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
-		return parameter.hasParameterAnnotation(CurrentMembers.class) && parameter.getParameterType().equals(Long.class);
+		return parameter.hasParameterAnnotation(CurrentMembers.class) && parameter.getParameterType()
+			.equals(Long.class);
 	}
 
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
+		CurrentMembers currentMembers = parameter.getParameterAnnotation(CurrentMembers.class);
+		boolean required = currentMembers != null && currentMembers.required();
+
 		HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-		if (request != null) {
-			Long membersId;
-			Cookie[] cookies = request.getCookies();
-			for (Cookie cookie : cookies) {
-				if ("Authorization".equals(cookie.getName())) {
-					try {
-						ResponseEntity<GetMembersTokenResponse> response = memberAuthClient.token();
-						membersId = response.getBody().membersId();
-						return membersId;
-					} catch (Exception e) {
-						e.printStackTrace();
-						return null;
-					}
+		if (request == null) {
+			if (required) {
+				throw new UnauthorizedException(ErrorCode.NOT_HTTP_REQUEST);
+			}
+			return null;
+		}
+
+		Long membersId = getMemberIdFromCookies(request.getCookies());
+		if (membersId == null && required) {
+			throw new UnauthorizedException(ErrorCode.UNAUTHORIZED);
+		}
+
+		return membersId;
+	}
+
+	private Long getMemberIdFromCookies(Cookie[] cookies) throws Exception {
+		if (cookies == null) {
+			return null;
+		}
+
+		for (Cookie cookie : cookies) {
+			if ("Authorization".equals(cookie.getName())) {
+				try {
+					ResponseEntity<GetMembersTokenResponse> response = memberAuthClient.token();
+					return Objects.requireNonNull(response.getBody()).membersId();
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+					e.printStackTrace();
 				}
 			}
 		}
