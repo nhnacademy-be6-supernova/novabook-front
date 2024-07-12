4a8ffd26d3450b8a4aa3c4f7cd67be208887a8f9
zaysverse <zeze@jbnu.ac.kr>
2024-07-12 17:43:00 +0900

    feat: rest 컨트롤러 추가

---
+++ src/main/java/store/novabook/front/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package store.novabook.front.common.handler;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+import store.novabook.front.common.exception.FeignClientException;
+import store.novabook.front.common.exception.NovaException;
+
+/**
+ * {@code GlobalExceptionHandler} 클래스는 애플리케이션 전역에서 발생하는 예외를 처리하는 핸들러입니다.
+ * 다양한 예외 유형에 대해 적절한 HTTP 상태 코드와 함께 응답을 반환합니다.
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+	@ExceptionHandler(NovaException.class)
+	public String handleFeignClientException(FeignClientException e, Model model) {
+		model.addAttribute("message", e.getMessage());
+		return "error/error";
+	}
+}
