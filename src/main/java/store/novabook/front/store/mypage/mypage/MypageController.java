8a5945a119857d119e599e59edb0b503a42dec70
zaysverse <zeze@jbnu.ac.kr>
2024-07-15 20:58:27 +0900

    feat: mypage 추가

--- src/main/java/store/novabook/front/store/mypage/mypage/MypageController.java
+++ src/main/java/store/novabook/front/store/mypage/mypage/MypageController.java
@@ -1,5 +1,6 @@
 package store.novabook.front.store.mypage.mypage;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import store.novabook.front.api.member.grade.service.MemberGradeService;
+import store.novabook.front.api.member.member.service.MemberCouponService;
 import store.novabook.front.api.order.service.OrdersBookService;
 import store.novabook.front.api.point.service.PointService;
 
@@ -17,6 +19,7 @@ public class MypageController {
 	private final MemberGradeService memberGradeService;
 	private final OrdersBookService ordersBookService;
 	private final PointService pointService;
+	private final MemberCouponService memberCouponService;
 	private static final int PAGE = 0;
 	private static final int SIZE = 2;
 
@@ -24,6 +27,7 @@ public class MypageController {
 	public String getMypage(Model model) {
 		model.addAttribute("pointHistories", pointService.getPointHistories(PAGE, SIZE));
 		model.addAttribute("orders", ordersBookService.getOrdersBookAll(PAGE, SIZE));
+		model.addAttribute("coupons", memberCouponService.getMyCouponHistoryAll(Pageable.ofSize(SIZE)));
 		model.addAttribute("grade", memberGradeService.getMemberGrade());
 		return "store/mypage/mypage_index";
 	}
