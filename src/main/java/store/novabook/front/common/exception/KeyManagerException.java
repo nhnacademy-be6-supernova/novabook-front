cf689b985390eb67e903ef9ecde7eda38e2aec7e
Hantaehwan <gksxoghkskr@naver.com>
2024-07-17 14:24:37 +0900

    #241 feat: 키매니저 에러처리

---
+++ src/main/java/store/novabook/front/common/exception/KeyManagerException.java
@@ -0,0 +1,7 @@
+package store.novabook.front.common.exception;
+
+public class KeyManagerException extends NovaException {
+	public KeyManagerException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+}
