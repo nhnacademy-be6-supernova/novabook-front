23b404bd12af813ae14850dfddea818a50aa7ea7
hyungwang <kfk10112@gmail.com>
2024-07-24 16:38:45 +0900

    fix: 로그인 쿠키 수정, 로그인쿠키유틸, 쓸모없는 validation 제

--- src/main/java/store/novabook/front/api/member/member/dto/request/IsExpireAccessTokenRequest.java
+++ src/main/java/store/novabook/front/api/member/member/dto/request/IsExpireAccessTokenRequest.java
@@ -1,9 +1,7 @@
 package store.novabook.front.api.member.member.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 
 public record IsExpireAccessTokenRequest(
-	@NotBlank
 	String accessToken
 ) {
 }
--- src/main/java/store/novabook/front/api/member/member/dto/response/GetMembersRoleRequest.java
+++ src/main/java/store/novabook/front/api/member/member/dto/response/GetMembersRoleRequest.java
@@ -1,9 +1,7 @@
 package store.novabook.front.api.member.member.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
 
 public record GetMembersRoleRequest(
-	@NotBlank
 	String accessToken
 ) {
 }
