d2554d1c52ca6d99f0b121a2392501125cdd64b6
ganghagyeong <gkrrod714@naver.com>
2024-07-31 11:05:28 +0900

    feat : 회원가입시 아이디 valid 수정 한글만 불가능

--- src/main/java/store/novabook/front/api/member/member/dto/request/CreateMemberRequest.java
+++ src/main/java/store/novabook/front/api/member/member/dto/request/CreateMemberRequest.java
@@ -10,7 +10,7 @@ import lombok.Builder;
 public record CreateMemberRequest(
 
 	@NotBlank(message = "아이디는 필수 입력 값입니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z0-9])[^\u3131-\uD79D]{6,20}$", message = "아이디는 영어 대소문자, 숫자 포함 최소 6자, 최대 20자여야 합니다. 한글은 포함될 수 없습니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z0-9])[a-zA-Z0-9_]{6,20}$", message = "아이디는 영어 대소문자, 숫자, 밑줄 포함 최소 6자, 최대 20자여야 합니다. 한글은 포함될 수 없습니다.")
 	String loginId,
 
 	@Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[~!@#$%^&*]).{10,16}$")
